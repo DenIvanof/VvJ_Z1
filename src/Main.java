92a09f303207ea7c0b7ee2f08a94becdc5ba2093
Ivanich <deniska090798@gmail.com>
2023-12-19 23:05:06 +0400

    main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x;
+        x = 13_676;
+        int y = 20;
+        int z = (x / y);
+
+        System.out.println("Рассчет количества начисленных миль за купленный билет.");
+        System.out.println("Стоимость билета на самолет " + (x) + "руб.");
+        System.out.println("Вам будет начислено за покупку билета " + (z) + "миль");
+
+
+    }
+}
