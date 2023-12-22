23a5f7dc746e8e27f37d09ab4613bf8e7f71a280
Ivanich <deniska090798@gmail.com>
2023-12-22 20:28:34 +0400

    add

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x;
-        x = 13_676;
-        int y = 20;
-        int z = (x / y);
+        int price;
+        price = 13_676;
+        int onemile = 20;
+        int miles = (price / onemile);
 
         System.out.println("Рассчет количества начисленных миль за купленный билет.");
-        System.out.println("Стоимость билета на самолет " + (x) + "руб.");
-        System.out.println("Вам будет начислено за покупку билета " + (z) + "миль");
+        System.out.println("Стоимость билета на самолет " + (price) + "руб.");
+        System.out.println("Вам будет начислено за покупку билета " + (miles) + "миль");
 
 
     }
