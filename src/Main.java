a972545341e98a3995f613eff3479144efde713c
Ivanich <deniska090798@gmail.com>
2023-12-26 22:12:24 +0400

    new

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int price;
-        price = 13_676;
-        int onemile = 20;
-        int miles = (price / onemile);
+        int price = 13_676;
+        int oneMile = 20;
+        int miles = (price / oneMile);
 
         System.out.println("Рассчет количества начисленных миль за купленный билет.");
         System.out.println("Стоимость билета на самолет " + (price) + "руб.");
